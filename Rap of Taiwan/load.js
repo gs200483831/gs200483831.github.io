bdb205bb1e52d86cf67da7a65c0b53c32952d6fc
gs200483831 <64567869+gs200483831@users.noreply.github.com>
2020-05-20 21:53:38 +0800

    shineAdd

--- Rap of Taiwan/load.js
+++ Rap of Taiwan/load.js
@@ -1,15 +1,12 @@
 // $('#load').addClass("load");
-
-setTimeout(function(){
-  $('.logbox').addClass("d-none");
-  // $('.main').removeClass("d-none");
+// $('.main').removeClass("d-none");
 //   if ($('.main').height() > $(window).height()) {
 //     $(".logbox").addClass("noscroll");
 // }else{
 //     $(".logbox").addClass("fixWindow"); 
 // }  
-$('body').removeClass("overflowhidden");
-},5000);
+setTimeout(function() {
+    $('.logbox').addClass("d-none");
 
-
-
+    $('body').removeClass("overflowhidden");
+}, 100);
