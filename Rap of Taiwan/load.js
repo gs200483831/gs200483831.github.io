645520efce5567ca5db3ccf944de03873406f4e3
gs200483831 <64567869+gs200483831@users.noreply.github.com>
2020-05-20 15:00:03 +0800

    animationAdd

---
+++ Rap of Taiwan/load.js
@@ -0,0 +1,15 @@
+// $('#load').addClass("load");
+
+setTimeout(function(){
+  $('.logbox').addClass("d-none");
+  // $('.main').removeClass("d-none");
+//   if ($('.main').height() > $(window).height()) {
+//     $(".logbox").addClass("noscroll");
+// }else{
+//     $(".logbox").addClass("fixWindow"); 
+// }  
+$('body').removeClass("overflowhidden");
+},5000);
+
+
+
